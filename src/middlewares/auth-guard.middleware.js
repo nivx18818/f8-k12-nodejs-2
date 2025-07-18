ed3b6a1673c8cd6c399498b2a1d37a5cc0e73946
NguyenXuanVinh <nongami87@gmail.com>
2025-07-18 22:10:28 +0700

    refactor(auth): use user service for token auth

--- src/middlewares/auth-guard.middleware.js
+++ src/middlewares/auth-guard.middleware.js
@@ -1,19 +1,16 @@
 const jwtService = require("@/services/jwt.service");
+const userService = require("@/services/user.service");
 const { User } = require("@/models");
 
-const authGuard = (req, res, next) => {
+const authGuard = async (req, res, next) => {
   const token = req.cookies.accessToken;
 
   if (!token) {
     return res.error(401, "Access token is required");
   }
 
-  const payload = jwtService.verify(token);
-
-  const user = User.findOne({
-    attributes: { exclude: ["password"] },
-    where: { id: payload.userId },
-  });
+  const { userId } = jwtService.verify(token);
+  const user = await userService.getById(userId);
 
   if (!user) {
     return res.error(401, "Invalid access token");
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -35,7 +35,7 @@ exports.refreshToken = async (token, data) => {
     return null;
   }
 
-  const newAccessToken = jwtService.sign({ userId: refreshToken.userId });
+  const newAccessToken = jwtService.sign(refreshToken.userId);
 
   const isSuccessful = await refreshTokenService.revoke(refreshToken.token);
   if (!isSuccessful) return null;
